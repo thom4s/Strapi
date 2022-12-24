34eefbbeadb3fd5a5d2058aee4eba664ae3e9bc5
thomas <hello@thomasflorentin.net>
2022-12-24 15:06:02 +0100

    fix devlop

--- src/api/exercice/controllers/exercice.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * exercice controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::exercice.exercice');
